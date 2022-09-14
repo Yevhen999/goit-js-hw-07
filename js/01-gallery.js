6e85b0fe7f259b0abbb285206b29f92638be8c19
Yevhen <leshchenkoyevhen@gmail.com>
2022-09-14 12:40:07 +0300

    start

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+
+const onGalleryRef = document.querySelector(".gallery");
+const onImgRef = document.querySelector(".gallery__image");
+
+const createdGalleryItem = galleryItems.map((item) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+});
+
+const createdGallery = createdGalleryItem.join("");
+
+onGalleryRef.insertAdjacentHTML("beforeend", createdGallery);
+
+onGalleryRef.addEventListener("click", handleRef);
+
+function handleRef(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  } else {
+  }
+}
+
+function onImageHandle() {
+  basicLightbox
+    .create(`<img width="1400" height="900" src="${item.original}">`)
+    .show();
+}
+
+onImageHandle();
+
+// document.querySelector("button.image").onclick = () => {
+//   basicLightbox
+//     .create(
+//       `
+// 		<img width="1400" height="900" src="${item.original}">
+// 	`
+//     )
+//     .show();
+// };
